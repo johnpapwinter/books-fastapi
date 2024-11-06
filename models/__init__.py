516e2f1514245701329c249bbf7d9e6b4177c263
johnpapwinter <johnpapahimonas@yahoo.com>
2024-11-06 13:39:31 +0200

    minor

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
-from .models import (BookRequest, SearchRequest, UserRequest, GenreRequest, BookResponse, GenreResponse,
-                     LoginRequest, LoginResponse, PaginatedResponse, ChangePasswordRequest)
+from .schemas import (BookRequest, SearchRequest, UserRequest, GenreRequest, BookResponse, GenreResponse,
+                      LoginRequest, LoginResponse, PaginatedResponse, ChangePasswordRequest)
 from .entities import Book, User, Genre
